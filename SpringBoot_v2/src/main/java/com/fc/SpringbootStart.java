a6584bfe01755371173fa4ab0bfd93a8a6fdc615
zsj <765390807@qq.com>
2021-04-03 18:03:53 +0800

    第一次提交一个文件

--- SpringBoot_v2/src/main/java/com/fc/SpringbootStart.java
+++ SpringBoot_v2/src/main/java/com/fc/SpringbootStart.java
@@ -14,7 +14,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class SpringbootStart {
 
     public static void main(String[] args) {
-
+//sjsj
         SpringApplication.run(SpringbootStart.class, args);
         System.out.println("*******************************************\n" + 
         		"* 码云地址                                                                                                        *\n" + 
